092afc6a83862e03cbc8a78e9d61efd126c1f7df
Ademilson <ademilsonfp@gmail.com>
2022-11-22 11:02:14 -0300

    schema improvements

--- schema.ts
+++ schema.ts
@@ -1,11 +1,19 @@
 
+/**
+ * Supported users
+ */
 export type User = (
   'developer' |
   'administrator' |
-  'operator'
+  'operator' |
+  'robot' |
+  'randomreader' |
+  'anonymous'
 );
 
 /**
+ * Supported numbers
+ *
  * @minimum -1
  * @maximum 2048
  * @multipleOf 1
@@ -13,20 +21,39 @@ export type User = (
 export type UserChoiceNumber = number;
 
 /**
+ * Supported words
+ *
  * @maxLength 8
+ * @pattern ^[a-z][a-z0-9]*$
  */
 export type UserChoiceWord = string;
 
+/**
+ * Supported user choices
+ */
 export type UserChoice = UserChoiceNumber | UserChoiceWord;
 
+/**
+ * Settings file
+ */
 export interface Settings {
+  /**
+   * Just assume it's true
+   */
   whatever: true,
+
+  /**
+   * What type of user are...
+   */
   you: User,
 
   /**
-   * Pick a list up to 5 numbers between -1 and 2048
+   * Pick a list up to 5 numbers or any word
    *
    * @maxItems 5
    */
   want?: UserChoice[]
-};
+}
+
+// For more information about supported TSDoc tags among JSON Schema specs, see:
+// https://github.com/vega/ts-json-schema-generator/blob/v1.1.2/src/AnnotationsReader/BasicAnnotationsReader.ts
